145b30204272b2b15e47602395c8ce0b8129de01
Ju Liu <ju.liu@welaika.com>
2014-06-22 22:47:05 +0200

    Fix songs controller create url

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,37 +1,33 @@
-module Api
-  module V1
-    class SongsController < ApplicationController
-      respond_to :json
+class Api::V1::SongsController < ApplicationController
+  respond_to :json
 
-      def index
-        respond_with Song.all
-      end
+  def index
+    respond_with Song.all
+  end
 
-      def show
-        respond_with song
-      end
+  def show
+    respond_with song
+  end
 
-      def create
-        respond_with Song.create(song_params)
-      end
+  def create
+    respond_with :api, :v1, Song.create(song_params)
+  end
 
-      def update
-        respond_with song.update(song_params)
-      end
+  def update
+    respond_with song.update(song_params)
+  end
 
-      def destroy
-        respond_with song.destroy
-      end
+  def destroy
+    respond_with song.destroy
+  end
 
-    private
+private
 
-      def song
-        Song.find(params[:id])
-      end
+  def song
+    Song.find(params[:id])
+  end
 
-      def song_params
-        params.require(:song).permit(:title, :text)
-      end
-    end
+  def song_params
+    params.require(:song).permit(:title, :text)
   end
 end
